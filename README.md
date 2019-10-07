4c82a4c11d458c6b3eef5d48ef174ef421a892bc
Gihovani Filipp Pereira Demétrio <gihovani@gmail.com>
2019-10-06 23:45:05 -0300

    Add Reset Password

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo
+- style guide
+- validações
--- test/functional/forgotpassword.spec.js
+++ test/functional/forgotpassword.spec.js
@@ -1,6 +1,8 @@
 const { test, trait } = use("Test/Suite")("Forgot Password");
 
 const Mail = use("Mail");
+const Hash = use("Hash");
+const { subHours, format } = require("date-fns");
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use("Factory");
@@ -11,29 +13,67 @@ const User = use("App/Models/User");
 trait("Test/ApiClient");
 trait("DatabaseTransactions");
 
+async function generateForgotPasswordToken(client, email) {
+  return token;
+}
+
 test("it should send an email with forgot password instructions", async ({
   assert,
   client
 }) => {
   Mail.fake();
 
-  const forgotPayload = {
-    email: "gihovani@gmail.com"
-  };
-  const user = await Factory.model("App/Models/User").create(forgotPayload);
+  const email = "gihovani@gmail.com";
+  const user = await Factory.model("App/Models/User").create({ email });
 
-  const response = await client
+  await client
     .post("/forgot")
-    .send(forgotPayload)
+    .send({ email })
     .end();
 
-  response.assertStatus(204);
-  const recentEmail = Mail.pullRecent();
-  assert.equal(recentEmail.message.to[0].address, forgotPayload.email);
-
   const token = await user.tokens().first();
-  console.log(token);
+  const recentEmail = Mail.pullRecent();
+  assert.equal(recentEmail.message.to[0].address, email);
 
-  assert.include(token.toJSON(), { user_id: user.id, type: "forgotpassword" });
+  assert.include(token.toJSON(), { type: "forgotpassword" });
   Mail.restore();
+});
+
+test("it should be able to reset password", async ({ assert, client }) => {
+  const email = "gihovani@gmail.com";
+  const user = await Factory.model("App/Models/User").create({ email });
+  const { token } = await Factory.model("App/Models/Token").create({
+    user_id: user.id,
+    type: "forgotpassword"
+  });
+
+  await client
+    .post("/reset")
+    .send({ token, password: "123456", password_confirmation: "123456" })
+    .end();
+
+  await user.reload();
+  const checkPassword = await Hash.verify("123456", user.password);
+
+  assert.isTrue(checkPassword);
+});
+
+test("it cannot reset password after 2h of forgot password request", async ({
+  client
+}) => {
+  const email = "gihovani@gmail.com";
+  const user = await Factory.model("App/Models/User").create({ email });
+  const token = await Factory.model("App/Models/Token").create({
+    user_id: user.id,
+    type: "forgotpassword"
+  });
+  token.created_at = format(subHours(new Date(), 5), "yyyy-MM-dd HH:ii:ss");
+  token.save();
+
+  const response = await client
+    .post("/reset")
+    .send({ token, password: "123456", password_confirmation: "123456" })
+    .end();
+
+  response.assertStatus(400);
 });
